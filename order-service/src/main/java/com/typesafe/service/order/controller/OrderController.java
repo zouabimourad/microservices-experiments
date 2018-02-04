4b74b61ef2e3d498c460c3cd3fc57820a496faf5
mourad.zouabi <zouabi.mourad@gmail.com>
2018-02-04 10:13:44 +0100

    reorg  build

--- order-service/src/main/java/com/typesafe/service/order/controller/OrderController.java
+++ order-service/src/main/java/com/typesafe/service/order/controller/OrderController.java
@@ -6,9 +6,11 @@ import com.typesafe.service.order.service.OrderService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.sleuth.annotation.NewSpan;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class OrderController {
